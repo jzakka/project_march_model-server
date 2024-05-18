08802911d65b2d8909f1c19f429a31d3e00861dd
Jzakka <mouse4786@gmail.com>
2024-05-18 13:20:04 +0900

    model 경로 외부 변수로 받도록 수정

--- service/model.py
+++ service/model.py
@@ -1,10 +1,12 @@
+import os
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoModel, AutoModelForSequenceClassification, TextClassificationPipeline
 
+MODEL_PATH=os.getenv("MODEL_PATH", "/root/kc_electra_unsmile")
 
 class NLPModel:
     def __init__(self):
-        self.model_modified = AutoModelForSequenceClassification.from_pretrained("/Users/chungsanghwa/projects/final-season/kc_electra_unsmile",local_files_only=True)
-        self.tokenizer = AutoTokenizer.from_pretrained("/Users/chungsanghwa/projects/final-season/kc_electra_unsmile", local_files_only = True)
+        self.model_modified = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH,local_files_only=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, local_files_only = True)
         self.pipe = TextClassificationPipeline( 
             model = self.model_modified,
             tokenizer = self.tokenizer,
