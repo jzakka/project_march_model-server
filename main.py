7746cd7e83d3b6394e21b3eff1a4d9fdb4e69eba
Daehyun Heo <hyeon365@gmail.com>
2024-05-26 20:14:20 +0900

    changed issues (classify() works on just one sentences, rounding)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
 from service.model import NLPModel
+
 app = FastAPI()
 
 model = NLPModel()
@@ -17,4 +18,8 @@ async def ping():
 async def classify(request: Request):
     body = await request.body()
     text_data = body.decode()
-    return JSONResponse(model.classify(text_content = text_data))
+    text_data = text_data.split(",")
+    # 복수개의 문장을 동시 처리할 수 있도록 구분자를 추가했습니다.
+    res = model.classify(text_content = text_data, batch_size=4)
+    #batch-size를 4개로 할당해 CPU 환경에서의 성능 향상을 고려했습니다.
+    return JSONResponse(res)
--- service/model.py
+++ service/model.py
@@ -1,17 +1,27 @@
 import os
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoModel, AutoModelForSequenceClassification, TextClassificationPipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
 
-MODEL_PATH=os.getenv("MODEL_PATH", "/root/kc_electra_unsmile")
+MODEL_PATH = os.getenv("MODEL_PATH", "/root/kc_electra_unsmile")
+
+#MODEL_PATH = os.getenv("MODEL_PATH", "C:/Users/허대현/tiny_git/NLPService/project_march_model-server/kc_electra_small_unified")
+#이 코드는 로컬 테스트 용입니다.
 
 class NLPModel:
     def __init__(self):
-        self.model_modified = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH,local_files_only=True)
+        self.model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH,local_files_only=True)
         self.tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, local_files_only = True)
         self.pipe = TextClassificationPipeline( 
-            model = self.model_modified,
+            model = self.model,
             tokenizer = self.tokenizer,
+            device=-1,
             return_all_scores=True,
+            function_to_apply='sigmoid'
         ) # 사용을 위한 파이프라인
     
+    
     def classify(self, text_content):
-        return self.pipe(text_content)[0]
+        results = self.pipe(text_content, batch_size=4)
+        for result in results:
+            for ls_pair in result:
+                ls_pair['score'] = round(ls_pair['score'], 4)
+        return results
